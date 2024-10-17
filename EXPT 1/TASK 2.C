590fe70cc2f241483e1ad31a5d10f4f04055444e
ashfaque26 <ashfaquesayyed299@eng.rizvi.edu.in>
2024-10-17 23:15:09 +0530

    Create TASK 2.C

---
+++ EXPT 1/TASK 2.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main() 
+{
+    printf("Student Details:\n\n");
+    printf("Name      :\t ASHFAQUE SAYYED\n");
+    printf("Age       :\t 19 YEARS OLD\n");
+    printf("Class     :\t FE\n");
+    printf("Division  :\t ECS E1\n");
+    printf("UIN       :\t 241S005");
+}
