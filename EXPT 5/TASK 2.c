cb2ac340adaf1bbd7fdcc7c32acaf5a0fe55402d
ashfaque26 <ashfaquesayyed299@eng.rizvi.edu.in>
2024-11-26 21:40:27 +0530

    Create TASK 2.c

---
+++ EXPT 5/TASK 2.c
@@ -0,0 +1,15 @@
+//WAP TO FIND FACTORIAL OF A NUMBER USING RECURSIVE
+
+#include<stdio.h>
+int main()
+ {
+  int i, num, factorial = 1;
+  printf("Enter a whole number to find Factorial = ");
+  scanf("%d", &num);
+  for (i = 1; i<=num; i++)
+   {
+    factorial = factorial * i;
+  }
+  printf("Factorial of %d is: %d", num, factorial);
+  return 0;
+}
